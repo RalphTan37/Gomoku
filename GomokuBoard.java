19790e3854f213c13ef1c09e1d977262c43d4fd7
Ed0827 <seyun.jeong@slu.edu>
2024-04-29 20:15:54 -0500

    implemented the condition

--- GomokuBoard.java
+++ GomokuBoard.java
@@ -1,8 +1,17 @@
 //Ralph is currently working on this
-import javax.swing.*;
-import java.awt.*;
+import static javax.swing.JOptionPane.*;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 public class GomokuBoard extends JFrame {
    //Instance Variables
@@ -80,6 +89,97 @@ public class GomokuBoard extends JFrame {
 			if (board[row][col] == '-') {
 				board[row][col] = 'X';
 				buttons[row][col].setText("X");
+				checkfiveinarow();
+			}
+		}
+		private void checkfiveinarow(){ 
+			for(int row = 0; row < size; ++row) {
+				int continuous = 0;
+				for(int col = 0; col < size; ++col){
+					if(board[row][col] == 'X') {
+						++continuous;
+						if(continuous == 5){
+							showMessageDialog(null,"Congrats you won Domoku");
+							return;
+						}
+					}
+					else {
+						continuous = 0;
+					}
+
+				}
+			}
+			for(int col = 0; col < size; ++col) {
+				int continuous = 0;
+				for(int row = 0; row < size; ++row){
+					if(board[row][col] == 'X') {
+						++continuous;
+						if(continuous == 5){
+							showMessageDialog(null,"Congrats you won Domoku");
+							return;
+						}
+					}
+					else {
+						continuous = 0;
+					}
+
+				}
+			}
+			for(int row = 0; row < size; ++row) {
+				for(int col = 0; col < size; ++col){
+					if(board[row][col] == 'X') {
+						int continuous = 0;
+						for(int offset = 0; offset < 5; ++offset){
+							int checkrow = row + offset;
+							if(checkrow >= size) {
+								break;
+							}
+							int checkcol = col + offset;
+							if(checkcol >= size){
+								break;
+							}
+							if(board[checkrow][checkcol] == 'X') {
+								++continuous;
+								if(continuous == 5){
+									showMessageDialog(null,"Congrats you won Domoku");
+									return;
+								}
+							}
+							else{
+								continuous = 0;
+							}
+						}
+					}
+
+				}
+			}
+			for(int row = 0; row < size; ++row) {
+				for(int col = 0; col < size; ++col){
+					if(board[row][col] == 'X') {
+						int continuous = 0;
+						for(int offset = 0; offset < 5; ++offset){
+							int checkrow = row + offset;
+							if(checkrow >= size) {
+								break;
+							}
+							int checkcol = col - offset;
+							if(checkcol >= size){
+								break;
+							}
+							if(board[checkrow][checkcol] == 'X') {
+								++continuous;
+								if(continuous == 5){
+									showMessageDialog(null,"Congrats you won Domoku");
+									return;
+								}
+							}
+							else{
+								continuous = 0;
+							}
+						}
+					}
+
+				}
 			}
 		}
 	}
