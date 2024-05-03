eda3c04d3e1c67f70767095f81a1ec881634ab56
unknown <ralphfedericktan@gmail.com>
2024-05-03 03:39:48 -0500

    Updating Progress

--- GomokuGame.java
+++ GomokuGame.java
@@ -1,7 +1,9 @@
 public class GomokuGame {
+	//Creates player 1 and 2 objects
     private GomokuPlayer player1 = new GomokuPlayer();
     private GomokuPlayer player2 = new GomokuPlayer();
     
+    //Handles players' turns and checks if the game is over
     public void startGame() {
     	while(!isGameOver()) {
     		char currentPlayerSymbol = getCurrentPlayerSymbol();
@@ -9,14 +11,17 @@ public class GomokuGame {
     	}
     }
     
+    //Checks for game condition
     private boolean isGameOver() {
     	return false;
     }
     
+    //Returns stone for current player
     private char getCurrentPlayerSymbol() {
     	return (player1.getCurrentPlayer() == 'O') ? 'O' : 'X';
     }
     
+    //Switches current player from player1 or player2
     private void switchPlayers() {
     	player1.switchPlayer();
     	player2.switchPlayer();
