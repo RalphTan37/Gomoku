dfdeb78ff935d78fab92f3b78e93619a933f75f0
unknown <ralphfedericktan@gmail.com>
2024-05-03 03:31:55 -0500

    Updating Progress

--- GomokuPlayer.java
+++ GomokuPlayer.java
@@ -2,10 +2,12 @@ public class GomokuPlayer {
     //Instance Variable
 	private char currentPlayer = 'O';
 	
+	//Returns current player's symbol
 	public char getCurrentPlayer() {
 		return currentPlayer;
 	}
 	
+	//Changing current player's symbol
 	public void switchPlayer() {
 		currentPlayer = (currentPlayer == 'O') ? 'X' : 'O';
 	}
